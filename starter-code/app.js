21194abf3006cf835c59e43369e92bf9e8a57939
Grazielli Pozzi <graziellipozzi@gmail.com>
2020-09-07 15:51:49 -0300

    partial-1

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the partials here:
 
 // add the routes here:
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.hbs');
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
